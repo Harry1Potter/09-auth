16d4998cc959e430bebab69db9c3462865009de3
Max T <maks34589@gmail.com>
2026-01-04 21:55:45 +0200

    commit message

--- proxy.ts
+++ proxy.ts
@@ -1,49 +1,11 @@
 import { NextRequest, NextResponse } from "next/server";
-import { cookies } from "next/headers";
-import { checkSession } from "@/lib/api/serverApi";
 
-export async function proxy(request: NextRequest) {
-  const { pathname } = request.nextUrl;
-
-  const cookieStore = await cookies();
-  const accessToken = cookieStore.get("accessToken")?.value;
-  const refreshToken = cookieStore.get("refreshToken")?.value;
-
-  const isAuthRoute =
-    pathname.startsWith("/sign-in") || pathname.startsWith("/sign-up");
-
-  const isPrivateRoute =
-    pathname.startsWith("/notes") || pathname.startsWith("/profile");
-
-  if (!accessToken && refreshToken) {
-    try {
-      await checkSession();
-      return NextResponse.next();
-    } catch {
-      const res = isPrivateRoute
-        ? NextResponse.redirect(new URL("/sign-in", request.url))
-        : NextResponse.next();
-
-      res.cookies.delete("accessToken");
-      res.cookies.delete("refreshToken");
-      return res;
-    }
-  }
-
-  if (isPrivateRoute && !accessToken) {
-    return NextResponse.redirect(new URL("/sign-in", request.url));
-  }
-
-  if (isAuthRoute && accessToken) {
-    return NextResponse.redirect(new URL("/", request.url));
-  }
-
+export function proxy(_request: NextRequest) {
   return NextResponse.next();
 }
 
 export const config = {
   matcher: [
-    "/profile/:path*",
     "/notes/:path*",
     "/sign-in",
     "/sign-up",
@@ -51,4 +13,3 @@ export const config = {
 };
 
 export default proxy;
-
